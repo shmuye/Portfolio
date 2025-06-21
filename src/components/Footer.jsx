7331d88ba637464735b7a6a6f6f4167ee5de0caf
Shmuye <shmuye27@gmail.com>
2025-06-21 16:07:22 +0300

    configured vite.config.js for gh-page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faFacebook } from "@fortawesome/free-brands-svg-icons"
+import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons"
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import { faTelegram } from "@fortawesome/free-brands-svg-icons"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
@@ -8,16 +8,16 @@ const Footer = () => {
     return (
         <div className="py-8 bg-[#222] text-blue-600 flex flex-col gap-8 w-full justify-center items-center sm:[45%]">
             <div className="flex gap-4 text-2xl">
-                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className=" text-white hover:text-blue-600 transition-colors duration-300">
-                    <FontAwesomeIcon icon={faFacebook} />
+                <a href="https://x.com/shmuye115933" target="_blank" rel="noopener noreferrer" className=" text-white hover:text-blue-600 transition-colors duration-300">
+                    <FontAwesomeIcon icon={faTwitterSquare} />
                 </a>
-                <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-700 transition-colors duration-300">
+                <a href="https://www.linkedin.com/in/shmuye-Ayalneh" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-700 transition-colors duration-300">
                     <FontAwesomeIcon icon={faLinkedin} />
                 </a>
                 <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-700 transition-colors duration-300">
                     <FontAwesomeIcon icon={faGithub} />
                 </a>
-                <a href="https://t.me/yourusername" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-500 transition-colors duration-300">
+                <a href="https://t.me/shudev27" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-500 transition-colors duration-300">
                     <FontAwesomeIcon icon={faTelegram} />
                 </a>
             </div>
